7cc662630058f05ba24750b3b1ea938117093045
Aman Yadav <72963272+Amanyadav183@users.noreply.github.com>
2021-08-22 23:50:08 +0530

    Add files via upload

---
+++ raksha.py
@@ -0,0 +1,143 @@
+from turtle import*
+color("black")
+pensize(4)
+speed(12)
+bgcolor("black")
+for i1 in range(1):
+    left(180)
+    forward(460)
+    left(180)
+for c1 in range(40):
+    color("blue")
+    pensize(0)
+    circle(4)
+    forward(9)
+    left(360)
+
+for i2 in range(1):
+    color("black")
+    left(180)
+    forward(460)
+    right(90)
+    forward(4)
+for i4 in range(1):
+    color("magenta")
+    pensize(3)
+    right(90)
+    forward(460)
+
+
+for i5 in range(1):
+    color("orange")
+    pensize(5)
+    left(45)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+
+for i6 in range(1):
+    color("black")
+    right(135)
+    forward(80)
+
+for j1 in range(1):
+    right(90)
+    forward(0)
+
+for c3 in range(1):
+    color("green")
+    pensize(20)
+    circle(10)
+
+for j2 in range(1):
+    left(90)
+    forward(53)
+
+for j3 in range(1):
+    right(90)
+    forward(0)
+
+for c2 in range(1):
+    color("red")
+    pensize(50)
+    circle(25)
+
+for j4 in range(1):
+    pensize(0)
+    left(90)
+    forward(85)
+
+for j5 in range(1):
+    right(90)
+    forward(0)
+
+
+for c4 in range(1):
+    color("green")
+    pensize(20)
+    circle(10)
+
+for y1 in range(1):
+    pensize(5)
+    left(90)
+    forward(30)
+    
+for j6 in range(1):
+    color("orange")
+    pensize(5)
+    left(45)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+
+for y2 in range(1):
+    color("black")
+    right(135)
+    forward(75)
+
+for c6 in range(40):
+    color("blue")
+    pensize(0)
+    circle(4)
+    forward(9)
+    left(360)
+
+for y3 in range(1):
+    color("black")
+    forward(80)
+    left(90)
+    forward(4)
+
+for y4 in range(1):
+    color("magenta")
+    pensize(3)
+    left(90)
+    forward(443)
+
+for y5 in range(1):
+    color("black")
+    right(90)
+    forward(200)
+    left(90)
+    forward(650)
+
+for y6 in range(1):
+          color("red")
+
+
+write("HAPPY RAKSHA BANDHAN",font = ("Chiller",60),move="true")
+
+
+
+    
+
+
+
